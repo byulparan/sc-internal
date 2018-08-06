59b541122a64b700a86ad521efa3d83b28acabce
byulparan <byulparan@gmail.com>
2018-08-06 11:43:52 +0900

    use standard library in C++(don't use pthread)

--- scsynth_add.cpp
+++ scsynth_add.cpp
@@ -2,7 +2,8 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
-#include <pthread.h>
+#include <mutex>
+#include <condition_variable>
 #include "SC_WorldOptions.h"
 
 
@@ -19,12 +20,11 @@ extern "C" {
   struct World* make_world(struct WorldOptions*);
 }
 
-
 char* lisp_buffer;
 
-pthread_mutex_t mutex;
-pthread_cond_t  cond_var1;
-pthread_cond_t  cond_var2;
+std::recursive_mutex mutex;
+std::condition_variable_any cond_var1;
+std::condition_variable_any cond_var2;
 
 int sbcl_printf(const char *fmt, va_list ap) {
   vprintf(fmt, ap);
@@ -34,40 +34,38 @@ int sbcl_printf(const char *fmt, va_list ap) {
 
 void communicate_init(char* _buffer) {
   lisp_buffer = _buffer;
-  pthread_mutex_init(&mutex, nullptr);
-  pthread_cond_init(&cond_var1, nullptr);
-  pthread_cond_init(&cond_var2, nullptr);
 }
 
 void sbcl_lock() {
-  pthread_mutex_lock(&mutex);
+  mutex.lock();
 }
 
 void sbcl_unlock() {
-  pthread_mutex_unlock(&mutex);
+  mutex.unlock();
 }
 void sbcl_wait_signal() {
-  pthread_cond_wait(&cond_var1, &mutex);
+  std::unique_lock<std::recursive_mutex> lk(mutex);
+  mutex.unlock();
+  cond_var1.wait(lk);
+  mutex.lock();
 }
 
 void sbcl_send_signal() {
-  pthread_cond_signal(&cond_var2);
+  cond_var2.notify_one();
 }
 
 void sbcl_quit_signal() {
-  pthread_cond_signal(&cond_var1);
+  cond_var1.notify_one();
 }
 
 void sbcl_reply_func (struct ReplyAddress *inReplyAddr, char* inBuf, int inSize) {
-  pthread_mutex_lock(&mutex);
+  std::unique_lock<std::recursive_mutex> lk(mutex);
   memset(lisp_buffer, 0, 1024);
   memcpy(lisp_buffer, inBuf, inSize);
   sbcl_message_size = inSize;
-  pthread_cond_signal(&cond_var1);
-  pthread_cond_wait(&cond_var2, &mutex);
-  pthread_mutex_unlock(&mutex);
+  cond_var1.notify_one();
+  cond_var2.wait(lk);
 }        
-
 
 struct World* make_world(struct WorldOptions* opt) {
   return World_New(opt);
