3c09962d38531ce054aacceb7aad97e6b827b866
Sungmin Park <byulparan@gmail.com>
2025-11-01 00:17:57 +0900

    add link.lisp

---
+++ link.lisp
@@ -0,0 +1,86 @@
+(in-package #:sc)
+
+(cffi:defcfun (link-enable "LinkEnable") :void
+  (bpm :float))
+(cffi:defcfun (link-disable "LinkDisable") :void)
+(cffi:defcfun (link-get-tempo "LinkGetTempo") :float)
+(cffi:defcfun (%link-set-tempo "LinkSetTempo") :void
+  (bpm :float))
+(cffi:defcfun (link-get-time "LinkGetTime") :double)
+(cffi:defcfun (link-get-beat "LinkGetBeat") :double)
+(cffi:defcfun (%link-get-beat-to-time "LinkGetBeatToTime") :double
+  (beat :double))
+(cffi:defcfun (%link-get-time-to-beat "LinkGetTimeToBeat") :double
+  (time :double))
+
+
+(defun link-set-tempo (bpm &key relaunch lag)
+  (declare (ignore relaunch lag))
+  (%link-set-tempo (float bpm 1.0)))
+
+(defun link-get-beat-to-time (beat)
+  (%link-get-beat-to-time (float beat 1.0d0)))
+
+(defun link-get-time-to-beat (time)
+  (%link-get-time-to-beat (float time 1.0d0)))
+
+
+(defclass link-clock (tempo-clock)
+  ())
+
+(defmethod beats-to-secs ((tempo-clock link-clock) beats)
+  (link-get-beat-to-time beats))
+
+(defmethod secs-to-beats ((tempo-clock link-clock) secs)
+  (link-get-time-to-beat secs))
+
+(defmethod tempo-clock-beats ((tempo-clock link-clock))
+  (link-get-beat))
+
+(defmethod tempo-clock-set-bpm ((tempo-clock link-clock) new-bpm)
+  (link-set-tempo new-bpm)
+  (call-next-method))
+
+
+(defun sync-timeoffset ()
+  (let* ((diff .0)
+	 (min-diff 10000000)
+	 (offset 0.0))
+    (dotimes (i 8)
+      (let* ((before-time (core-audio-time))
+	     (link-time (link-get-time))
+	     (after-time (core-audio-time)))
+	(setf diff (- after-time before-time))
+	(when (< diff min-diff)
+	  (setf min-diff diff)
+	  (setf offset (- link-time (+ before-time (* diff .5)))))))
+    offset))
+
+(defun link-clock (flag)
+  (stop)
+  (if flag (progn
+	     (link-enable (sc-extensions:bpm))
+	     (setf (host-offset *s*) (sync-timeoffset))
+	     (pushnew #'link-set-tempo sc-extensions::*bpm-functions*)
+	     (pushnew #'link-disable  *server-quit-hooks*)
+	     (set-clock (make-instance 'sc::link-clock
+			  :name "LinkClock"
+			  :server *s*
+			  :timestamp #'link-get-time
+			  :bpm (link-get-tempo)))
+	     (sc-extensions:bpm (link-get-tempo)))
+    (progn
+      (setf (host-offset *s*) 0)
+      (set-clock (make-instance 'sc::tempo-clock
+		   :name (sc::name *s*)
+		   :server *s*
+		   :timestamp #'core-audio-time
+		   :bpm (sc-extensions:bpm)))
+      (alexandria::removef sc-extensions::*bpm-functions* #'link-set-tempo)
+      (alexandria:removef *server-quit-hooks* #'link-disable)
+      (link-disable))))
+
+(export 'link-clock)
+
+
+
--- sc-internal.asd
+++ sc-internal.asd
@@ -1,8 +1,10 @@
 (asdf/defsystem:defsystem #:sc-internal
   :depends-on (#:cl-collider
+	       #:sc-extensions
 	       #:static-vectors
 	       #:trivial-main-thread)
   :serial t
   :components ((:file "ffi-type")
-	       (:file "internal-server")))
+	       (:file "internal-server")
+	       (:file "link")))
 
